451b1dd079a896ea3cff1494c7b85708c4622767
Carlos12323 <129915131+Carlos12323@users.noreply.github.com>
2023-05-21 12:10:53 -0500

    Add files via upload

---
+++ src/Entidades/Arquero.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Arquero extends Personaje {
+    
+    private int agilidad;
+    private int precision;
+    
+    public void dispararFlecha(Personaje atacado){
+        //Implementar
+    }
+
+    @Override
+        public void atacar(Personaje atacado){
+           int nivelActualDeVida = atacado.getVida();
+           if(this.getPrecision()>5)
+               atacado.setVida(nivelActualDeVida - 50);         
+           else
+               atacado.setVida(nivelActualDeVida - 6);
+           atacado.defender(this);
+        }
+
+    /**
+     * @return the agilidad
+     */
+    public int getAgilidad() {
+        return agilidad;
+    }
+
+    /**
+     * @param agilidad the agilidad to set
+     */
+    public void setAgilidad(int agilidad) {
+        this.agilidad = agilidad;
+    }
+
+    /**
+     * @return the precision
+     */
+    public int getPrecision() {
+        return precision;
+    }
+
+    /**
+     * @param precision the precision to set
+     */
+    public void setPrecision(int precision) {
+        this.precision = precision;
+    }
+    }
+     
